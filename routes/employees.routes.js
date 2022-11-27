1cf2dd4d4ac1c85a2ecc821b0fc4a4c499b1102b
Farook <farook.jintha@ace-alternatives.com>
2022-11-27 12:26:09 +0530

    removed auth in employees

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -5,15 +5,15 @@ const { isAdmin } = require('../utils/authorizationMiddleware');
 
 const router = express.Router();
 
-router.get('/:userID/employees', getEmployees);
+router.get('/employees', getEmployees);
 
-router.get('/:userID/employees/:empID', getEmployeeByID);
+router.get('/employees/:empID', getEmployeeByID);
 
-router.post('/:userID/employees',  addEmployee);
+router.post('/employees',  addEmployee);
 
-router.put('/:userID/employees/:empID', updateEmployee);
+router.put('/employees/:empID', updateEmployee);
 
-router.delete('/:userID/employees/:empID', deleteEmployee);
+router.delete('/employees/:empID', deleteEmployee);
 
 router.param('userID', getLoggedUserByID);
 
